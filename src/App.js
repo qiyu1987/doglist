ec150def67a0031cb001165c969982da7b2be314
qiyu1987 <qiyu1987@gmail.com>
2019-10-02 14:22:02 +0200

    dog details finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DogsList from './components/Dogslist';
+import DogsListContainer from './components/DogslistContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <DogsListContainer />
+      </main>
     </div>
   );
 }
