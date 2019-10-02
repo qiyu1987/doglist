27403b53d8968f0fb3f1c6488e06c043bf8f08c7
qiyu1987 <qiyu1987@gmail.com>
2019-10-02 15:18:23 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom'
 import DogsList from './components/Dogslist';
 import DogsListContainer from './components/DogslistContainer';
+import DogBreedImages from './components/DogBreedImages'
+import DogBreedImagesContainer from './components/DogBreedImagesContainer';
 
 function App() {
   return (
     <div className="App">
       <main>
-        <DogsListContainer />
+      <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
+      <Route exact path="/" component={DogsListContainer} />
       </main>
     </div>
   );
